c34edf19d506e170d3cd28085714e96eaf9037fc
779208739 <lhf779208739@gmail.com>
2022-03-12 17:44:33 +0800

    Update coroutine_DB.go
    Definition  of some data structure & test

--- coroutine_DB.go
+++ coroutine_DB.go
@@ -5,8 +5,10 @@ import (
 )
 
 type Data struct {
+	val int
 }
 type DataTable struct {
+	DataList []Data
 }
 type Lock struct {
 	mode int
@@ -15,6 +17,31 @@ type LockTable struct {
 	Table []Lock
 }
 
+func (t *DataTable) createData(val int) {
+	d := Data{val}
+
+	t.DataList = append(t.DataList, d)
+
+}
+
+func (t *DataTable) deleteData(line int) bool {
+	if line < 0 || line > cap(t.DataList) {
+		fmt.Println("Warming: Invalid:line", line)
+		return false
+	}
+
+	tmpList := t.DataList[:line-1]
+	tmpList2 := t.DataList[line:]
+	t.DataList = append(tmpList, tmpList2...)
+
+	return true
+}
 func main() {
-	fmt.Println("My favorite number is 1232")
+	var Dt DataTable
+	Dt.createData(222)
+	Dt.createData(233)
+	Dt.createData(244)
+	Dt.deleteData(10)
+	fmt.Println(Dt.DataList)
+
 }
